4528cab537575be002fb6fd282f538ceb470d1db
Rajaniraiyn R <72294760+Rajaniraiyn@users.noreply.github.com>
2023-07-12 15:12:08 +0530

    add clarity

--- theme.config.tsx
+++ theme.config.tsx
@@ -20,7 +20,15 @@ const config: DocsThemeConfig = {
     defaultMenuCollapseLevel: 1,
     toggleButton: true,
   },
-  head: <>{/* HEAD TAG */}</>,
+  head: <>
+    <script type="text/javascript">
+      {`(function (c, l, a, r, i, t, y) {
+        c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+      t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+      y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+      })(window, document, "clarity", "script", "h19w430f3j");`}
+    </script>
+  </>,
   primaryHue: {
     dark: 180,
     light: 220,
